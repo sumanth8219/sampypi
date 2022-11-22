b1de02e655bee1aa24ada5b6e3fb9c69e52d83eb
Sumanth Madyalkar <sumanth.v.m@in.ibm.com>
2022-11-22 21:30:39 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+
+setup(
+    name="testpy",
+    version="0.0.1",
+    author="Sumanth",
+    author_email="sumanthvm.techie@gmail.com",
+    description="A package to convert your Jupyter Notebook",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sumanth8219/sampypi",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
